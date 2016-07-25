d800a2484407d6cccd6a22ef81721055c0b271eb
Bernhard Grünewaldt <github@gruenewaldt.net>
2016-07-25 20:39:17 +0200

    initial deploy script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # node-deploy-essentials
 Node.js Deploy Essential Scripts
+
+### THIS IS WORK IN PROGRESS - DO NOT USE
+
+## Deploy to GitHub Pages
+
+Will deploy to GitHub Pages with the following command
+
+```
+npm run ndes deployToGitHubPages as "John Smith" withEmail "john@something.foo" withGitHubAuthUsername john123 withGitHubAuthToken aaa121411f31f31ff13 toRepository https://github.com/john123/johntest.git fromSource "./build/" intoSubdirectory "customdir" 
+```
+
+  * needs to have `git` installed
+
+
+## License
+
+[MIT License ](./LICENSE.md) 
---
+++ scripts/deployToGitHubPages.js
@@ -0,0 +1,57 @@
+var exports = module.exports = {};
+var shell = require('shelljs');
+var CLONE_DIR = 'github-pages-to-deploy';
+
+exports.run = function(gitHubCommitterEmail,
+                       gitHubCommitterName,
+                       gitHubAuthUsername,
+                       gitHubAuthTokenOrPassword,
+                       gitHubCloneUrl,
+                       gitHubSubdirectory,
+                       sourceDirToDeployContents) {
+    console.log('running');
+
+    var gitVersion = shell.exec('git --version', {silent:true}).stdout;
+
+    console.log('gitVersion: ' + gitVersion);
+
+    shell.exec('git config --global user.email "' + gitHubCommitterEmail + '"', {silent:true});
+    shell.exec('git config --global user.name "' + gitHubCommitterName + '"', {silent:true});
+    shell.exec('git config --global push.default simple', {silent:true});
+
+    // TODO: REPLACE 'https://' here with ' https://user:token@'
+
+    //
+    // CLONE
+    //
+    shell.exec('git clone --single-branch --branch gh-pages ' + gitHubCloneUrl + ' ' + CLONE_DIR, {silent:true});
+
+    //
+    // CREATE SUBDIR IF NECESSARY
+    //
+    var workspace = CLONE_DIR;
+    if (gitHubSubdirectory !== undefined && gitHubSubdirectory !== null) {
+        if (! shell.test('-d', CLONE_DIR + '/' + gitHubSubdirectory)) {
+            shell.mkdir(CLONE_DIR + '/' + gitHubSubdirectory);
+            workspace = CLONE_DIR + '/' + gitHubSubdirectory;
+        }
+    }
+
+    //
+    // COPY BUILD ARTIFACTS
+    //
+    shell.rm('-rf', workspace + '/*');
+    shell.cp('-r', sourceDirToDeployContents + '/*', workspace + '/');
+
+    shell.cd(workspace);
+    shell.exec('git diff --exit-code', function(code, stdout, stderr) {
+        if (code === 0) {
+            shell.echo("NOTHING TO COMMIT");
+        } else {
+            shell.echo("CHANGES DETECTED. ADDING, COMMITTING, PUSHING.");
+            shell.exec('git add . -A');
+            shell.exec('git commit -m "automatic deploy by node-deploy-essentials script"');
+            shell.exec('git push');
+        }
+    });
+};
